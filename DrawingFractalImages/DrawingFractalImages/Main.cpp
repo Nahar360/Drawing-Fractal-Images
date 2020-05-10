cc7d9ee236986314923ce846da07ee742c8fcaea
Joaquin <joaquinollerogarcia@gmail.com>
2020-05-10 11:53:24 +0100

    Using histogram values to paint the Mandelbrot fractal.

--- DrawingFractalImages/DrawingFractalImages/Main.cpp
+++ DrawingFractalImages/DrawingFractalImages/Main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstdint>
+#include <memory>
+#include "math.h"
 
 #include "Bitmap.hpp"
 #include "Mandelbrot.hpp"
@@ -12,26 +14,69 @@ int main()
 
 	const int WIDTH = 800;
 	const int HEIGHT = 600;
+	const int SIZE = WIDTH * HEIGHT;
 
 	Bitmap bitmap(WIDTH, HEIGHT);
 
-	double min = INT_MAX;
-	double max = INT_MIN;
+	std::unique_ptr<int[]> histogram(new int[Mandelbrot::MAX_ITERATIONS]);
+	for (int i = 0; i < Mandelbrot::MAX_ITERATIONS; i++)
+	{
+		histogram[i] = 0;
+	}
 
+	std::unique_ptr<int[]> fractal(new int[SIZE]);
+	for (int i = 0; i < SIZE; i++)
+	{
+		fractal[i] = 0;
+	}
+
+	// Storing
 	for (int y = 0; y < HEIGHT; y++)
 	{
 		for (int x = 0; x < WIDTH; x++)
 		{
-			double xFractal = (x - (WIDTH / 2)) * 2.0 / WIDTH;
+			double xFractal = (x - (WIDTH / 2) - 175) * 2.0 / HEIGHT; // same scaling factor (HEIGHT)
 			double yFractal = (y - (HEIGHT / 2)) * 2.0 / HEIGHT;
 
 			int iterations = Mandelbrot::getIterations(xFractal, yFractal);
-			uint8_t red = (uint8_t)(256 * (double)iterations / Mandelbrot::MAX_ITERATIONS);
+			if (iterations != Mandelbrot::MAX_ITERATIONS)
+			{
+				histogram[iterations]++;
+			}
 
-			bitmap.setPixel(x, y, red, red, red);
+			fractal[y * WIDTH + x] = iterations;
+		}
+	}
 
-			if (red < min) min = red;
-			if (red > max) max = red;
+	int total = 0;
+	for (int i = 0; i < Mandelbrot::MAX_ITERATIONS; i++)
+	{
+		total += histogram[i];
+	}
+
+	// Drawing
+	for (int y = 0; y < HEIGHT; y++)
+	{
+		for (int x = 0; x < WIDTH; x++)
+		{
+			uint8_t red = 0;
+			uint8_t green = 0;
+			uint8_t blue = 0;
+
+			int iterations = fractal[y * WIDTH + x];
+			if (iterations != Mandelbrot::MAX_ITERATIONS)
+			{
+				double hue = 0.0;
+
+				for (int i = 0; i <= iterations; i++)
+				{
+					hue += ((double)histogram[i]) / total;
+				}
+
+				green = std::pow(255, hue);
+			}
+
+			bitmap.setPixel(x, y, red, green, blue);
 		}
 	}
 
